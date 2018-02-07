baef0958fb4742b2eed5f73ae64e94372b4a7830
Dan Bohea <git@bohea.co.uk>
2018-02-07 20:12:48 +0000

    Now using rsync in create_fork_files.sh to keep travis happy

--- scripts/create_fork_files.sh
+++ scripts/create_fork_files.sh
@@ -8,23 +8,17 @@ DIR="$(dirname "$0")"
 cd "$DIR"
 cd ../
 
-# Copy and rename files from ./src:
-if [ $TRAVIS ]; then
-  setStatusMessage "Creating ./config.yml if absent"
-  rsync src/config.example.yml config.yml --ignore-existing
-  setStatusMessage "Creating ./config.local.yml if absent"
-  rsync src/config.example.yml config.local.yml --ignore-existing
-  setStatusMessage "Creating ./mac-custom.yml if absent"
-  rsync src/mac-custom.example.yml mac-custom.yml --ignore-existing
-  setStatusMessage "Creating ./requirements.yml if absent"
-  rsync src/requirements.example.yml requirements.yml --ignore-existing
-else
-  setStatusMessage "Creating ./config.yml if absent"
-  cp -n src/config.example.yml config.yml
-  setStatusMessage "Creating ./config.local.yml if absent"
-  cp -n src/config.example.yml config.local.yml
-  setStatusMessage "Creating ./mac-custom.yml if absent"
-  cp -n src/mac-custom.example.yml mac-custom.yml
-  setStatusMessage "Creating ./requirements.yml if absent"
-  cp -n src/requirements.example.yml requirements.yml
-fi
+# Copy and rename files from ./src
+# We're using `rsync` instead of `cp` because for some reason Travis now fails
+# when using `cp -n` (it's the `-n` it doesn't like).
+setStatusMessage "Creating ./config.yml if absent"
+rsync src/config.example.yml config.yml --ignore-existing
+
+setStatusMessage "Creating ./config.local.yml if absent"
+rsync src/config.example.yml config.local.yml --ignore-existing
+
+setStatusMessage "Creating ./mac-custom.yml if absent"
+rsync src/mac-custom.example.yml mac-custom.yml --ignore-existing
+
+setStatusMessage "Creating ./requirements.yml if absent"
+rsync src/requirements.example.yml requirements.yml --ignore-existing
