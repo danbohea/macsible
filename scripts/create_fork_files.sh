cfb809abdc2bd4766037545b8b11ac461998e20a
Dan Bohea <git@bohea.co.uk>
2018-02-07 17:15:56 +0000

    Debugging travis

--- scripts/create_fork_files.sh
+++ scripts/create_fork_files.sh
@@ -5,19 +5,19 @@ DIR="$(dirname "$0")"
 . "$DIR/common.sh"
 
 # Change to project root directory
-cd "$DIR"
-cd ../
+# cd "$DIR"
+# cd ../
 
 # Copy and rename files from ./src:
 
-setStatusMessage "Creating ./config.yml if absent"
-cp -n src/config.example.yml config.yml
-
-setStatusMessage "Creating ./config.local.yml if absent"
-cp -n src/config.example.yml config.local.yml
-
-setStatusMessage "Creating ./mac-custom.yml if absent"
-cp -n src/mac-custom.example.yml mac-custom.yml
-
-setStatusMessage "Creating ./requirements.yml if absent"
-cp -n src/requirements.example.yml requirements.yml
+# setStatusMessage "Creating ./config.yml if absent"
+# cp -n src/config.example.yml config.yml
+#
+# setStatusMessage "Creating ./config.local.yml if absent"
+# cp -n src/config.example.yml config.local.yml
+#
+# setStatusMessage "Creating ./mac-custom.yml if absent"
+# cp -n src/mac-custom.example.yml mac-custom.yml
+#
+# setStatusMessage "Creating ./requirements.yml if absent"
+# cp -n src/requirements.example.yml requirements.yml
